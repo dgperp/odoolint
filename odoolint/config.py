9b4efb2da850fb66b2292f99ba82f265ffc0ff05
macair <44501285+qlimited@users.noreply.github.com>
2024-08-18 20:28:00 +0700

    [IMP] improve code end of line

--- odoolint/config.py
+++ odoolint/config.py
@@ -5,7 +5,7 @@ DEFAULT_CONFIG = {
     "flake8_select": "C,E,F,W,B,B9,N801,N803",
     "flake8_ignore": "E203,E501,W503,C901,W605,E722,E731",
     "flake8_exclude": ["**unported**", "**__init__.py", "tests"],
-    "check_file_types": [".xml", ".js", ".css", ".scss"]
+    "check_file_types": [".xml", ".js", ".css", ".scss", "csv"]
 }
 
 
